905e09a72f91d6551b64bfb0879b0d61258bc34f
CtrAtlDelete <kvasovski34@gmail.com>
2022-12-09 00:17:14 +0300

    fix

--- hash-collisions-CtrAtlDel/Program.cs
+++ hash-collisions-CtrAtlDel/Program.cs
@@ -4,12 +4,12 @@
     {
         public static void Main(string[] args)
         {
-            var size = 3; //size in bytes
-            var sizeOfWord = 4;
-            var shaXX = new ShaXx(size);
+            var size = 4; // size of return byte[] sha256. Trim sha256
+            var sizeOfWord = 4; // 
+            var shaXX = new ShaXx(size); // 
             var randomArray = shaXX.RandomByteGenerator(sizeOfWord);
             Algorythm.HappyBirthday(size, randomArray);
             Console.Out.WriteLine();
         }
     }
-};
+}
